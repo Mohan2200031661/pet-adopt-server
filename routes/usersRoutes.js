49c420ad5b13d8e78e45eecc645632d1ef26970c
Thammineni Mohan Raj Kumar <116945231+Mohan2200031661@users.noreply.github.com>
2024-04-27 07:09:57 +0530

    Update usersRoutes.js

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 const userController = require("../controllers/userController");
